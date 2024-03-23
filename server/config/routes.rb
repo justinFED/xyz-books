d3f966105b2fe30750749e6b141e0757ff4d29b3
Justin Cantillo <justin.tech96@gmail.com>
2024-03-23 22:30:32 +0800

    Fixed ISBN10 routing books

--- server/config/routes.rb
+++ server/config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   get '/books/convert/:isbn', to: 'books#convert_isbn', constraints: { isbn: /.*/ }
 
-  get '/books/:isbn', to: 'books#show', constraints: { isbn: /\d{1}-\d{3}-\d{5}-\d{1}|[0-9X]{3}-[0-9X]{1,5}-[0-9X]{1,7}-[0-9X]{1,7}-\d/ }
+  get '/books/:isbn', to: 'books#show', constraints: { isbn: /\d{1}-\d{3}-\d{5}-\d{1}|[0-9X]{3}-[0-9X]{1,5}-[0-9X]{1,7}-[0-9X]{1,7}-\d|[0-9X]{1}-[0-9X]{3}-[0-9X]{5}-[0-9X]{1}/ }
 end
