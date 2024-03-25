42c0e1f03ff14a8c71bee77f1adccdefde70931a
Justin Cantillo <justin.tech96@gmail.com>
2024-03-26 02:10:41 +0800

    Fixed isbn validation

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,18 +1,18 @@
 import React from "react";
-import { BrowserRouter as Router } from "react-router-dom"; // Import BrowserRouter
+import { BrowserRouter as Router } from "react-router-dom";
 import HeaderBar from "./components/Header/HeaderBar";
 import HeroSection from "./components/HeroSection/HeroSection";
 import backgroundImage from "./assets/bg.jpg";
-import BookPage from "./components/BookPage";
+import SearchBook from "./components/SearchBook";
 
 function App() {
   return (
-    <Router> {/* Wrap your entire application with Router */}
+    <Router>
       <div className="flex flex-col min-h-screen">
         <HeaderBar />
         <main className="flex-1 bg-top" style={{ backgroundImage: `url(${backgroundImage})`, backgroundSize: 'cover' }}>
           <HeroSection />
-          <BookPage />
+          <SearchBook />
         </main>
         <footer className="bg-blue-500 text-center py-4">
           Footer
