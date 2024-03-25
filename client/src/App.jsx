55510e64c304ce2a23e1cb92dda869a0d3654ad6
Justin Cantillo <justin.tech96@gmail.com>
2024-03-26 00:45:19 +0800

    Fixed fetching data

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,19 +1,24 @@
 import React from "react";
+import { BrowserRouter as Router } from "react-router-dom"; // Import BrowserRouter
 import HeaderBar from "./components/Header/HeaderBar";
 import HeroSection from "./components/HeroSection/HeroSection";
 import backgroundImage from "./assets/bg.jpg";
+import BookPage from "./components/BookPage";
 
 function App() {
   return (
-    <div className="flex flex-col min-h-screen" >
-      <HeaderBar />
-      <main className="flex-1 bg-top" style={{ backgroundImage: `url(${backgroundImage})`, backgroundSize: 'cover' }}>
-        <HeroSection />
-      </main>
-      <footer className="bg-blue-500 text-center py-4">
-        Footer
-      </footer>
-    </div>
+    <Router> {/* Wrap your entire application with Router */}
+      <div className="flex flex-col min-h-screen">
+        <HeaderBar />
+        <main className="flex-1 bg-top" style={{ backgroundImage: `url(${backgroundImage})`, backgroundSize: 'cover' }}>
+          <HeroSection />
+          <BookPage />
+        </main>
+        <footer className="bg-blue-500 text-center py-4">
+          Footer
+        </footer>
+      </div>
+    </Router>
   );
 }
 
---
+++ client/src/components/BookPage.jsx
@@ -0,0 +1,92 @@
+import React, { useState } from "react";
+import Header from "./Header/Header";
+import { useNavigate } from "react-router-dom";
+
+function BookPage() {
+  const [bookData, setBookData] = useState(null);
+  const [searchInput, setSearchInput] = useState("");
+  const [error, setError] = useState("");
+  const [loading, setLoading] = useState(false); 
+  const navigate = useNavigate();
+
+  const isValidISBN = (isbn) => {
+    return isbn.length === 10 || isbn.length === 13;
+  };
+
+  const fetchBookData = async (isbn) => {
+    try {
+      setLoading(true); 
+      setError(""); 
+  
+      const response = await fetch(`http://127.0.0.1:3000/books/${isbn}`);
+      if (!response.ok) {
+        throw new Error(`HTTP error! Status: ${response.status}`);
+      }
+   
+      const data = await response.json();
+      setBookData(data);
+  
+    } catch (error) {
+      console.error("Error fetching book data:", error);
+      setError("An error occurred while fetching book data.");
+    } finally {
+      setLoading(false); 
+    }
+  };
+  
+
+  const handleSearch = async () => {
+    try {
+      setError("");
+      const isbn = searchInput;
+      if (!isValidISBN(isbn)) {
+        setError("Invalid ISBN");
+        return;
+      }
+      await fetchBookData(isbn); 
+    } catch (error) {
+      console.error("Error fetching book data:", error);
+      setError("An error occurred while fetching book data.");
+    }
+  };
+  
+  return (
+    <div>
+      <Header />
+      <div className="container mx-auto">
+        <div className="py-8">
+          <div className="flex justify-center">
+            <input
+              className="appearance-none bg-transparent border border-gray-300 rounded w-64 py-2 px-4 text-gray-700 leading-tight focus:outline-none"
+              type="text"
+              placeholder="Enter ISBN"
+              value={searchInput}
+              onChange={(e) => setSearchInput(e.target.value)}
+            />
+            <button
+              className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded ml-2"
+              onClick={handleSearch}
+            >
+              Search
+            </button>
+          </div>
+          {error && <p className="text-red-500">{error}</p>}
+          {loading && <p>Loading...</p>} 
+          {bookData && !loading && (
+            <div>
+              <h2 className="text-xl font-semibold mb-2">{bookData.title}</h2>
+              <p>Authors: {bookData.authors.split(', ').join(', ')}</p>
+              <p>Edition: {bookData.edition}</p>
+              <p>Price: PHP {bookData.price}</p>
+              <p>ISBN: {bookData.isbn_13}</p>
+              <p>Publication Year: {bookData.publication_year}</p>
+              <p>Publisher: {bookData.publisher}</p>
+            </div>
+          )}
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default BookPage;
