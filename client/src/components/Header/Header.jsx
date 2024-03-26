c292444a16fa1857d1f26797517b80189e30bfbc
Justin Cantillo <justin.tech96@gmail.com>
2024-03-27 02:08:29 +0800

    Added assets

--- client/src/components/Header/Header.jsx
+++ client/src/components/Header/Header.jsx
@@ -1,9 +1,27 @@
-import React from "react";
+import backgroundImage from "../../assets/MaskGroup4.png";
+import imageBefore from "../../assets/26.svg";
+import imageAfter from "../../assets/26.svg";
 
 function Header() {
   return (
-    <div className=" text-center p-2 text-white font-medium text-sm bg-gradient-to-r from-cyan-500 to-blue-500 ">
-      <p>Save up to $500 per year on millions of book titles!</p>
+    <div
+      className="text-center p-2 text-white font-medium text-sm flex items-center justify-center"
+      style={{
+        backgroundImage: `url(${backgroundImage})`,
+        backgroundSize: "cover"
+      }}
+    >
+      <div style={{ display: "flex", alignItems: "center" }}>
+        <img
+          src={imageBefore}
+          alt="Before"
+          style={{ marginRight: "5px", transform: "scaleX(-1)" }}
+        />
+        <p className="px-2">
+          Save up to $500 per year on millions of book titles!
+        </p>
+        <img src={imageAfter} alt="After" style={{ marginLeft: "5px" }} />
+      </div>
     </div>
   );
 }
