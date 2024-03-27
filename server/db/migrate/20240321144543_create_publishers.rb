5840fdf85fbda7224fe39cd7a915c1475d9690b4
Justin Cantillo <justin.tech96@gmail.com>
2024-03-28 00:53:47 +0800

    Added rspec and created test for models

--- server/db/migrate/20240321144543_create_publishers.rb
+++ server/db/migrate/20240321144543_create_publishers.rb
@@ -1,8 +1,7 @@
 class CreatePublishers < ActiveRecord::Migration[7.1]
   def change
     create_table :publishers do |t|
-      t.string :name, null: false  # Defines a column named 'name' of type string. The 'null: false' option ensures that a name must be provided for each publisher.
-      t.timestamps  # Automatically adds 'created_at' and 'updated_at' columns to store timestamps for record creation and update.
-    end  # Ends the 'create_table' block.
-  end  # Ends the 'change' method.
-end  # Ends the migration class definition.
+      t.string :name, null: false 
+    end
+  end
+end 
