81ae04e4ac6d818dd3e373d91570e986a89c3a96
Justin Cantillo <justin.tech96@gmail.com>
2024-03-31 03:46:49 +0800

    FAQ component

--- client/src/components/FAQ/FAQ.jsx
+++ client/src/components/FAQ/FAQ.jsx
@@ -1,25 +1,54 @@
 import { useState } from "react";
 
 const FAQ = () => {
-  const [isOpen, setIsOpen] = useState(false);
+  const [isOpen1, setIsOpen1] = useState(false);
+  const [isOpen2, setIsOpen2] = useState(false);
+  const [isOpen3, setIsOpen3] = useState(false);
+  const [isOpen4, setIsOpen4] = useState(false);
 
-  const toggleAccordion = () => {
-    setIsOpen(!isOpen);
+  const toggleAccordion1 = () => {
+    setIsOpen1(!isOpen1);
+    setIsOpen2(false);
+    setIsOpen3(false);
+    setIsOpen4(false);
+  };
+
+  const toggleAccordion2 = () => {
+    setIsOpen2(!isOpen2);
+    setIsOpen1(false);
+    setIsOpen3(false);
+    setIsOpen4(false);
+  };
+
+  const toggleAccordion3 = () => {
+    setIsOpen3(!isOpen3);
+    setIsOpen1(false);
+    setIsOpen2(false);
+    setIsOpen4(false);
+  };
+
+  const toggleAccordion4 = () => {
+    setIsOpen4(!isOpen4);
+    setIsOpen2(false);
+    setIsOpen3(false);
+    setIsOpen1(false);
   };
 
   return (
-    <section className=" bg-[#f1f5f8]  w-full  py-8 md:py-16 ">
-      <div className="  container px-44 mx-auto flex flex-col justify-center md:flex md:justify-center">
+    <section className="bg-[#f1f5f8] w-full py-8 md:py-16">
+      <div className="container px-44 mx-auto flex flex-col justify-center md:flex md:justify-center">
         <h1 className="text-3xl font-bold">Frequently Asked Questions</h1>
-        <div className="flex flex-col  justify-center  w-full mt-8 ">
-          <div className="border-b border-gray-300 py-4  ">
+        <div className="flex flex-col justify-center w-full mt-8">
+          <div className="border-b border-gray-300 py-4">
             <button
-              className="flex justify-between items-center w-full  py-2 text-lg font-semibold text-[#6060f4] bg-transparent  focus:outline-none"
-              onClick={toggleAccordion}
+              className="flex justify-between items-center w-full py-2 text-lg font-semibold text-[#6060f4] bg-transparent focus:outline-none"
+              onClick={toggleAccordion1}
             >
               Dedicated customer support and help portal.
               <svg
-                className={`w-6 h-6 ${isOpen ? "transform rotate-180" : ""}`}
+                className={`w-6 h-6 transition-transform duration-300 transform ${
+                  isOpen1 ? "rotate-180" : ""
+                }`}
                 xmlns="http://www.w3.org/2000/svg"
                 fill="none"
                 viewBox="0 0 24 24"
@@ -33,8 +62,8 @@ const FAQ = () => {
                 />
               </svg>
             </button>
-            {isOpen && (
-              <p className="py-2 ">
+            {isOpen1 && (
+              <p className="py-2">
                 With our dedicated customer support team, you can rest easy
                 knowing that we're doing everything we can to save you time,
                 money, and stress.
@@ -43,12 +72,14 @@ const FAQ = () => {
           </div>
           <div className="border-b border-gray-300 py-4">
             <button
-              className="flex justify-between items-center w-full  py-2 text-lg font-semibold text-[#6060f4] bg-transparent focus:outline-none"
-              onClick={toggleAccordion}
+              className="flex justify-between items-center w-full py-2 text-lg font-semibold text-[#6060f4] bg-transparent focus:outline-none"
+              onClick={toggleAccordion2}
             >
               Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed
               <svg
-                className={`w-6 h-6 ${isOpen ? "transform rotate-180" : ""}`}
+                className={`w-6 h-6 transition-transform duration-300 transform ${
+                  isOpen2 ? "rotate-180" : ""
+                }`}
                 xmlns="http://www.w3.org/2000/svg"
                 fill="none"
                 viewBox="0 0 24 24"
@@ -62,7 +93,7 @@ const FAQ = () => {
                 />
               </svg>
             </button>
-            {isOpen && (
+            {isOpen2 && (
               <p className="py-2">
                 Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed
               </p>
@@ -70,12 +101,14 @@ const FAQ = () => {
           </div>
           <div className="border-b border-gray-300 py-4">
             <button
-              className="flex justify-between items-center w-full  py-2 text-lg font-semibold text-[#6060f4] bg-transparent focus:outline-none"
-              onClick={toggleAccordion}
+              className="flex justify-between items-center w-full py-2 text-lg font-semibold text-[#6060f4] bg-transparent focus:outline-none"
+              onClick={toggleAccordion3}
             >
               Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed
               <svg
-                className={`w-6 h-6 ${isOpen ? "transform rotate-180" : ""}`}
+                className={`w-6 h-6 transition-transform duration-300 transform ${
+                  isOpen3 ? "rotate-180" : ""
+                }`}
                 xmlns="http://www.w3.org/2000/svg"
                 fill="none"
                 viewBox="0 0 24 24"
@@ -89,21 +122,22 @@ const FAQ = () => {
                 />
               </svg>
             </button>
-            {isOpen && (
+            {isOpen3 && (
               <p className="py-2">
                 Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed
               </p>
             )}
           </div>
-
           <div className="border-b border-gray-300 py-4">
             <button
-              className="flex justify-between items-center w-full  py-2 text-lg font-semibold text-[#6060f4] bg-transparent focus:outline-none"
-              onClick={toggleAccordion}
+              className="flex justify-between items-center w-full py-2 text-lg font-semibold text-[#6060f4] bg-transparent focus:outline-none"
+              onClick={toggleAccordion4}
             >
               Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed
               <svg
-                className={`w-6 h-6 ${isOpen ? "transform rotate-180" : ""}`}
+                className={`w-6 h-6 transition-transform duration-300 transform ${
+                  isOpen4 ? "rotate-180" : ""
+                }`}
                 xmlns="http://www.w3.org/2000/svg"
                 fill="none"
                 viewBox="0 0 24 24"
@@ -117,7 +151,7 @@ const FAQ = () => {
                 />
               </svg>
             </button>
-            {isOpen && (
+            {isOpen4 && (
               <p className="py-2">
                 Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed
               </p>
