b84ee664f290ea2bb6974f66a5cf7b2745d95b0b
Justin Cantillo <justin.tech96@gmail.com>
2024-03-31 03:10:40 +0800

    Shop and testimonial component

--- client/src/components/ExploreBooks/Testimonial.jsx
+++ client/src/components/ExploreBooks/Testimonial.jsx
@@ -4,25 +4,29 @@ import Star from "../../assets/star.svg";
 
 function Testimonial() {
   return (
-    <section className="   w-full  py-8 md:py-16 ">
-      <div className="   container my-14 px-14 mx-auto flex flex-col items-center justify-center md:flex md:justify-center">
+    <section className="   w-full h-screen py-8 md:py-16 ">
+      <div className="   container px-14 mx-auto flex flex-col items-center justify-center md:flex md:justify-center">
         <div className="  flex flex-col items-center justify-center ">
           <h1 className="text-3xl font-bold">Andrew Santellan</h1>
           <p className="text-lg text-[#63cef5]">Product Manager, Binance</p>
           <div className="flex items-center space-x-2 mt-7 text-gray-500">
-            <img src={Binance} alt="Image 1" className="w-[30px] h-auto" />
+            <img src={Binance} alt="Image 1" className="w-[40px] h-auto" />
             <p className="text-2xl">Binance</p>
           </div>
         </div>
+        
+        <div className="bg-[#4099f5] w-9/12 text-white  mt-24 container mx-auto p-16 rounded-2xl" 
+        style={{ boxShadow: '0px -48px 0px -22px rgba(217, 234, 252, 255)' }}>
 
-        <div className="bg-[#4099f5] w-9/12 text-white  mt-24 container mx-auto p-16 rounded-lg shadow-lg">
-          <div className="flex items-center justify-center relative ">
+           
+          <div className=" flex items-center justify-center ">
             <img
               src={User}
               alt="Image 1"
-              className="w-[110px] h-auto object-cover absolute  left-1/2 transform -translate-x-1/2 -translate-y-1/2"
-            />
+              className="w-[120px] h-auto absolute left-1/2 transform -translate-x-1/2 -translate-y-1/2"
+            />    
           </div>
+         
 
           <div className="text-2xl leading-relaxed mt-8 text-center items-center flex flex-col space-y-14">
             <p>
